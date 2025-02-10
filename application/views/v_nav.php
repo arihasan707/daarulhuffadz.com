42983b61bfd831394eba1aa2bdf639c69949f1c8
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2025-02-10 16:02:32 +0700

    fix nav link

--- application/views/v_nav.php
+++ application/views/v_nav.php
@@ -3,7 +3,7 @@
         <div class="row justify-content-xl-center align-items-center">
             <div class="col-xl-2 col-8 tp-sticky-column">
                 <div class="tp-sticky-logo">
-                    <a href="<?= base_url('beranda')?>"><img src="<?= base_url(); ?>assets/frontend/img/logo/dhi.png"
+                    <a href="<?= base_url('beranda') ?>"><img src="<?= base_url(); ?>assets/frontend/img/logo/dhi.png"
                             width="200px" class="img-fluid" alt="logo not found"></a>
                 </div>
             </div>
@@ -12,25 +12,25 @@
                     <div class="tp-main-menu bg-white">
                         <nav id="tp-mobile-menu">
                             <ul class="text-center">
-                                <li class="menu-item-has-children"><a href="<?= base_url();?>" class="b">Beranda</a>
+                                <li class="menu-item-has-children"><a href="<?= base_url(); ?>" class="b">Beranda</a>
                                 </li>
                                 <li class="menu-item-has-children"><a href="#">Tentang Kami</a>
                                     <ul class="sub-menu">
                                         <!-- <li><a href="<?= base_url('tentang/komite_pengurus/'); ?>">Komite Pengurus</a>
                                         </li> -->
-                                        <li><a href="<?= base_url('tentang/daftar_cabang/'); ?>">Daftar Cabang</a></li>
+                                        <li><a href="<?= base_url('tentang/daftar_cabang'); ?>">Daftar Cabang</a></li>
                                     </ul>
                                 </li>
                                 <li class="menu-item-has-children p"><a
-                                        data-id="<?= base_url('pmb_online/pendaftaran/'. $slug['slug']); ?>"
-                                        data-id2="<?= base_url('pmb_online/detail_program/'. $slug['slug']); ?>"
-                                        href="<?= base_url('pmb_online/'); ?>">PMB Online</a>
+                                        data-id="<?= base_url('pmb_online/pendaftaran/' . $slug['slug']); ?>"
+                                        data-id2="<?= base_url('pmb_online/detail_program/' . $slug['slug']); ?>"
+                                        href="<?= base_url('pmb_online'); ?>">PMB Online</a>
                                 </li>
-                                <li><a href="<?= base_url('dhi_news/') ?>" class="b">DHI News</a></li>
-                                 <li><a href="<?= base_url('galeri') ?>" class="b">Galeri</a></li>
-                                <li><a href="<?= base_url('biaya/') ?>" class="b">Biaya</a></li>
-                                <li><a href="<?= base_url('kontak/') ?>" class="k">Kontak</a></li>
-                                <li><a href="<?= base_url('faqs/') ?>" class="f">FAQs</a></li>
+                                <li><a href="<?= base_url('dhi_news') ?>" class="b">DHI News</a></li>
+                                <li><a href="<?= base_url('galeri') ?>" class="b">Galeri</a></li>
+                                <li><a href="<?= base_url('biaya') ?>" class="b">Biaya</a></li>
+                                <li><a href="<?= base_url('kontak') ?>" class="k">Kontak</a></li>
+                                <li><a href="<?= base_url('faqs') ?>" class="f">FAQs</a></li>
                             </ul>
                         </nav>
                     </div>
